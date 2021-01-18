de240e34a49d7cd303ee78545aaccf49784d561b
nadiaalkarina <nadiaalkarina@gmail.com>
2021-01-18 20:47:45 +0700

    add new update

--- Object Repository/Attendance/PunchOut.rs
+++ Object Repository/Attendance/PunchOut.rs
@@ -10,7 +10,7 @@
    <followRedirects>false</followRedirects>
    <httpBody></httpBody>
    <httpBodyContent>{
-  &quot;text&quot;: &quot;{\n  \&quot;datetime\&quot; : \&quot;09-02-2021\&quot;\n}&quot;,
+  &quot;text&quot;: &quot;{\n  \&quot;id\&quot; : \&quot;2\&quot;,\n  \&quot;timezone\&quot; : \&quot;London\&quot;,\n  \&quot;note\&quot; : \&quot;Masuk\&quot;,\n  \&quot;datetime\&quot; : \&quot;09-02-2021\&quot;\n}&quot;,
   &quot;contentType&quot;: &quot;application/json&quot;,
   &quot;charset&quot;: &quot;UTF-8&quot;
 }</httpBodyContent>
